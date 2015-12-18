29f02c126a66d2351658c039083c1c3b0367e457
valeriy <epsm@i.ua>
2015-12-18 19:50:43 +0200

    mend
    test case for GeneraTionScheduleValidator estimated

--- src/test/java/com/yvhobby/epsm/model/dispatch/GenerationScheduleValidatorTest.java
+++ src/test/java/com/yvhobby/epsm/model/dispatch/GenerationScheduleValidatorTest.java
@@ -1,5 +1,31 @@
 package test.java.com.yvhobby.epsm.model.dispatch;
 
 public class GenerationScheduleValidatorTest {
-
+	public void powerInGenerationCurveTooHighForGenerator(){
+		
+	}
+	
+	public void powerInGenerationCurveTooLowForGenerator(){
+		
+	}
+	
+	public void astaticRegulationTurnedOffAndThereIsNoGenerationCurve(){
+		
+	}
+	
+	public void generationCurveHasLessThan24hours(){
+		
+	}
+	
+	public void generationCurveHasMoreThan24hours(){
+		
+	}
+	
+	public void numbersOfGeneratorsOnStationConformsToTheirNubersInSchedule(){
+		
+	}
+	
+	public void scheduleContainsTheSameFeneratorsIdAsPowerStation(){
+		
+	}
 }
